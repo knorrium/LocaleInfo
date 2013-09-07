a107363112f1249734ee4b3d9bad6ea21b621ccd
Felipe Knorr Kuhn <git@knorrium.info>
2013-09-07 13:29:11 -0700

    added the needed outlets

--- LocaleInfo/LIViewController.h
+++ LocaleInfo/LIViewController.h
@@ -9,5 +9,9 @@
 #import <UIKit/UIKit.h>
 
 @interface LIViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *txtTextField;
+@property (weak, nonatomic) IBOutlet UITextView *txtTextView;
+@property (weak, nonatomic) IBOutlet UILabel *lblLocale;
+@property (weak, nonatomic) IBOutlet UILabel *lblCountry;
 
 @end
